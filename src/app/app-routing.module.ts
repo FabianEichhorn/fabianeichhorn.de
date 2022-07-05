4d4ace369283e4cc79e536a94ee6844aac49154c
FabianEichhorn <fabihorn.go@gmail.com>
2022-07-05 14:32:12 +0200

    added my-worksection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { DataProtectionComponent } from './data-protection/data-protection.compo
 import { ImprintComponent } from './imprint/imprint.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { SkillsComponent } from './skills/skills.component';
+import { MyWorkComponent } from './my-work/my-work.component';
 
 const routes: Routes = [
   { path: '', component: SlideshowComponent },
   { path: '', component: SkillsComponent},
+  { path: '', component: MyWorkComponent},
   { path: 'imprint', component: ImprintComponent },
   { path: 'data-protection', component:DataProtectionComponent },
 ];
